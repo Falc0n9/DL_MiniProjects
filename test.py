42fb967219a3be86fa0377c53e02f39446cb08db
Michiel Van Beirendonck <Michiel.vanbeirendonck@student.kuleuven.be>
2018-05-15 23:17:39 +0200

    Cleaned up test.py

--- test.py
+++ test.py
@@ -1,70 +1,44 @@
-import torch
 from torch import Tensor,FloatTensor, LongTensor
 from mini_project_2 import *
-import numpy
 from math import sqrt, pi
 import matplotlib.pyplot as plt
 from plot import plot_data
 from dlc_practical_prologue import convert_to_one_hot_labels
 
 train_data = Tensor(10000,2).uniform_()
-train_target = LongTensor([int(sqrt((train_data[i][0])**2 + (train_data[i][1])**2) < 1/sqrt(2*pi)) for i in range(len(train_data))])
+train_target = (train_data.pow(2).sum(1) < 1 / (2 * pi)).long()
 train_target_one_hot = convert_to_one_hot_labels(train_data,train_target)
 
-train_data_for_plot = train_data
-train_data = train_data.pow(2).sum(1).unsqueeze(0).t()
+seq = Sequential(Linear(2,25),
+                 ReLU(),
+                 Linear(25, 25),
+                 ReLU(),
+                 Linear(25, 25),
+                 ReLU(),
+                 Linear(25, 2))
 
-#print(train_data)
-#plot_data(train_data, train_target)
-
-
-fc_input = Linear(1, 2)
-relu1 = Tanh()
-fc_hidden_1 = Linear(2, 25)
-relu2 = Tanh()
-fc_hidden_2 = Linear(25, 25)
-relu3 = Tanh()
-fc_hidden_3 = Linear(25, 25)
-relu4 = Tanh()
-fc_output = Linear(25, 2)
-
-seq = Sequential(fc_input,
-                 relu1,
-                 fc_hidden_1,
-                 relu2,
-                 fc_hidden_2,
-                 relu3,
-                 fc_hidden_3,
-                 relu4,
-                 fc_output)
-
-lr = 0.001
-nb_epochs = 50
-batch_size = 10
+lr, nb_epochs, batch_size = 0.01, 50, 10
 
 loss_list = []
-
-for i in range(0,nb_epochs):
+for e in range(0,nb_epochs):
+    sum_loss = 0
     for b in range(0, train_data.size(0), batch_size):
 
-        input = train_data.narrow(0,b,batch_size)
+        data = train_data.narrow(0, b, batch_size)
         target = train_target_one_hot.narrow(0,b,batch_size)
 
         seq.zero_grad()
-        loss, loss_grad = loss_mse(seq.forward(input), target)
+        loss, loss_grad = loss_mse(seq.forward(data), target)
         seq.backward(loss_grad)
-        for param in seq.param():
-            param.data -= lr * param.grad
-    loss_list += [loss]
+        sum_loss = sum_loss + loss
+        for p in seq.param():
+            p.data.sub_(lr * p.grad)
+    loss_list += [sum_loss]
 
 plt.plot(loss_list)
 plt.show()
 
 output = seq.forward(train_data)
 _, predicted_classes = output.max(1)
-
-plot_data(train_data_for_plot,predicted_classes)
-
-#for param in seq.param():
-#    print(param.data)
+plot_data(train_data, predicted_classes)
 
